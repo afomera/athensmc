8b53f5897e318060c87fa68a9bcd195744747847
Andrea Fomera <afomera@hey.com>
2021-08-23 23:01:51 -0600

    Add back sucker_punch and email config for production

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module AthensMC
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # Use sucker_punch for delayed jobs
+    config.active_job.queue_adapter = :sucker_punch
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
