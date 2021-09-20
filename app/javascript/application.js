dfc9bd3fec42814273cd3829edf36062d1ec0ae3
Andrea Fomera <afomera@hey.com>
2021-09-19 22:58:01 -0600

    Add jsbundling and remove all old actioncable or non stimulus code from functioning, but use esbuild for BS and turbo JS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,26 @@
-import * as bootstrap from "bootstrap"
+require("@rails/ujs").start();
+
+import "@hotwired/turbo-rails";
+import "bootstrap";
+
+// import RconConsole from "app/javascript/servers/RconConsole";
+
+// class Dispatcher {
+//   constructor() {
+//     this.pageName = document.body.dataset.page;
+//   }
+
+//   route() {
+//     switch (this.pageName) {
+//       case "admin:servers:show":
+//         new RconConsole().init();
+//         break;
+//     }
+//   }
+// }
+
+// document.addEventListener("turbo:load", () => {
+//   const dispatcher = new Dispatcher();
+
+//   dispatcher.route();
+// });
