6519d239a926466192ce7caffbed6462182dec75
Andrea Fomera <afomera@hey.com>
2021-09-24 22:17:20 -0600

    Add localtime

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,27 +1,8 @@
 require("@rails/ujs").start();
 
+import LocalTime from "local-time"
+LocalTime.start()
+
 import "@hotwired/turbo-rails";
 import "bootstrap";
 import "trix";
-
-// import RconConsole from "app/javascript/servers/RconConsole";
-
-// class Dispatcher {
-//   constructor() {
-//     this.pageName = document.body.dataset.page;
-//   }
-
-//   route() {
-//     switch (this.pageName) {
-//       case "admin:servers:show":
-//         new RconConsole().init();
-//         break;
-//     }
-//   }
-// }
-
-// document.addEventListener("turbo:load", () => {
-//   const dispatcher = new Dispatcher();
-
-//   dispatcher.route();
-// });
